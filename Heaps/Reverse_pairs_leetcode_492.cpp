49ab326f36cc2972ee5dd7f01906501628abfad8
Razi <Khanrazi002@gmail.com>
2025-12-30 06:20:15 +0530

    Reverse-Pairs

---
+++ Heaps/Reverse_pairs_leetcode_492.cpp
@@ -0,0 +1,51 @@
+#include<vector>
+#include<iostream>
+#include<queue>
+using namespace std;
+class Solution {
+public:
+    int count = 0;
+
+    void merge(vector<int> &nums , int left , int mid , int right){
+        vector<int> temp;
+        int i = left , j = mid + 1;
+        while(i <= mid && j <= right ){
+            if(nums[i] <= nums[j]){
+                temp.push_back(nums[i++]);
+            }
+            else{
+                temp.push_back(nums[j++]);
+            }
+        }
+        while(i <= mid ) temp.push_back(nums[i++]);
+        while(j <= right ) temp.push_back(nums[j++]);
+        for(int k = left ; k <= right ; k++){
+            nums[k] = temp[k-left];
+        }
+    }
+
+    void countpairs(vector<int> &nums , int left , int mid , int right){
+        int j = mid + 1;
+        for(int i = left ; i <= mid ; i++){
+            while(j <= right && (long long)nums[i] > 2LL * nums[j]){
+                j++;
+            }
+            count += j - (mid + 1);
+        }
+    }
+
+    void MergeSort(vector<int> &nums , int left , int right){
+        if(left >= right) return;
+        int mid = left + (right - left)/2;
+        MergeSort(nums , left , mid);
+        MergeSort(nums , mid + 1 , right);
+        countpairs(nums , left , mid , right);
+        merge(nums , left , mid , right);
+    }
+
+
+    int reversePairs(vector<int>& nums) {
+        MergeSort(nums , 0 , nums.size()-1);
+        return count;
+    }
+};
